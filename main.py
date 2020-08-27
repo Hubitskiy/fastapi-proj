2ab2fc65ec19032d43988f0f870cb44bd6b55590
Hubitskiy <dmitry.g@upservice.io>
2020-08-27 23:51:04 +0300

    add get request

--- main.py
+++ main.py
@@ -4,6 +4,28 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-def root():
-    return {"message": "Hello World!"}
+list_of_films_by_genre = {
+    "detective": [
+        "Seven",
+        "Sleven Lucky number",
+        "Sherlock Holmes"
+    ],
+    "criminal": [
+        "Pulp fiction",
+        "God Father",
+        "Once Upon Timer in America"
+    ],
+    "action": [
+        "Lethal Weapon",
+        "Die Hard",
+        "Inglourious Basterds"
+    ]
+}
+
+
+@app.get("/films/{genre}")
+def get_list_films_by_genre(genre: str):
+    return list_of_films_by_genre.get(genre,
+        {
+            "Error": "Films doesnt found"
+        })

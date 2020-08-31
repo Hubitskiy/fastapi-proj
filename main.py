44dfd63083278813b9b9a09596364cc90c4fddbf
Hubitskiy <dmitry.g@upservice.io>
2020-09-01 00:00:13 +0300

    query_params

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from fastapi import FastAPI
 
+from enum import Enum
+
+
+class FilmsStore(str, Enum):
+    detective = "detective"
+    criminal = "criminal"
+    action = "action"
+
 
 app = FastAPI()
 
@@ -8,24 +16,29 @@ list_of_films_by_genre = {
     "detective": [
         "Seven",
         "Sleven Lucky number",
-        "Sherlock Holmes"
+        "Sherlock Holmes",
+        "Prestige",
+        "Death Note"
     ],
     "criminal": [
         "Pulp fiction",
         "God Father",
-        "Once Upon Timer in America"
+        "Once Upon Timer in America",
+        "Joker",
+        "Professional"
     ],
     "action": [
         "Lethal Weapon",
         "Die Hard",
-        "Inglourious Basterds"
+        "Inglourious Basterds",
+        "007",
+        "Batman"
     ]
 }
 
 
 @app.get("/films/{genre}")
-def get_list_films_by_genre(genre: str):
-    return list_of_films_by_genre.get(genre,
-        {
-            "Error": "Films doesnt found"
-        })
+def get_list_films_by_genre(genre: FilmsStore, skip: int = 0, limit: int = 25):
+    return {
+        "films": list_of_films_by_genre.get(genre.name)[skip: skip + limit]
+    }

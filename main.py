8cac7a0e4aaeead5e6bf641a33362ad36f77402e
Hubitskiy <dmitry.g@upservice.io>
2020-09-01 00:34:08 +0300

    post requests

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+
+from typing import List
 
 from enum import Enum
+from pydantic import BaseModel
 
 
-class FilmsStore(str, Enum):
+class FilmsAddPayload(BaseModel):
+    films: List[str]
+
+
+class FilmsGenre(str, Enum):
     detective = "detective"
     criminal = "criminal"
     action = "action"
@@ -38,7 +45,12 @@ list_of_films_by_genre = {
 
 
 @app.get("/films/{genre}")
-def get_list_films_by_genre(genre: FilmsStore, skip: int = 0, limit: int = 25):
+def get_list_films_by_genre(genre: FilmsGenre, skip: int = 0, limit: int = 25):
     return {
         "films": list_of_films_by_genre.get(genre.name)[skip: skip + limit]
     }
+
+
+@app.post("/films/{genre}")
+def add_film_to_store(genre: FilmsGenre, filmspayload: FilmsAddPayload):
+    return list_of_films_by_genre[genre] + filmspayload.films
